6ebc14a1f590e5f60591dba22a9024b457365c71
Rutushree Palwekar <palwekar.rutushree@gmail.com>
2024-07-21 23:36:02 +0200

    unitTesting

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,3 +1,4 @@
+
 describe("Iteration 1 | Getting Started", () => {
     describe("Function add", () => {
       it("should be defined", () => {
@@ -19,6 +20,10 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it(" should return undefined if any of the two arguments is not a number", () => {
+        expect(add(1, "2")).toEqual(undefined);
+        expect(add("3", 4)).toEqual(undefined);
+        expect(add("100", "47")).toEqual(undefined);
+      })
     });
   });
-  
